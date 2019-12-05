b19df8932a96565a60c0d5d177ad985b6c93daa3
Maksims Rosmanuk <m.rosmanuk@gmail.com>
2019-12-05 14:28:28 +0200

    final xpath refactoring

--- src/test/java/www1alv/steps/Homepage.java
+++ src/test/java/www1alv/steps/Homepage.java
@@ -14,7 +14,7 @@ public class Homepage {
     private By fullScreenBanner = byXpath("//a[@class='close-button animated']");
     private By getFullScreenBannerFrame = By.xpath("//*[@id='mt-65cf2a318dbd5e21']");
     private By advSlider = By.xpath("//div[@class='close-button-slider desktop']");
-    private By advSliderFrame = By.xpath("//*[@id='mt-b832a55b5fb34368']");
+    private By advSliderFrame = By.xpath("//*[@id='mt-d8736f517de4c467']");
     private By cookieButton = By.xpath("//a[@class='c-button-inverse']");
     private By phoneCategory = By.xpath("//a[contains(@href,'telefoni_plansetdatori')]");
     private By sortBySelector = By.xpath("//a[@class='select-opener select-add-info']");
@@ -42,7 +42,7 @@ public class Homepage {
          Thread.sleep(4000);
          try{
              if($(advSliderFrame).isDisplayed()){
-                 switchTo().frame("mt-b832a55b5fb34368");
+                 switchTo().frame("mt-d8736f517de4c467");
                  $(advSlider).click();}
              else{
                  System.out.println("bottom slider not found");}}
